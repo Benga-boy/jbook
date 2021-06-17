a6f450f80802cd264f43d334287727a188a1ecef
Benga <benga.olasebikan@gmail.com>
2021-06-17 21:51:24 +0100

    esbundler setup complete

---
+++ src/components/FormElements.tsx
@@ -0,0 +1,61 @@
+import * as esbuild from 'esbuild-wasm'
+import { useRef } from 'react'
+import { useEffect } from 'react'
+import { useState } from "react"
+import { Fragment } from "react"
+import { unpkgPathPlugin } from '../plugin'
+
+
+
+const FormElements = () => {
+  const [input, setInput] = useState('')
+  const [code, setCode] = useState('')
+  const ref = useRef<any>()
+
+
+  useEffect(() => {
+    startService()
+  }, [])
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    })
+    
+  }
+
+  const handleClick = async () => {
+    const {current} = ref
+
+    if (!current) return
+
+    const result = await current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+      // define: {
+      //   'process.env.NODE_ENV': "prodduction",
+      //   global: 'window'
+      // }
+    })
+
+    // console.log(result)
+
+    setCode(result.outputFiles[0].text)
+    
+  }
+
+  return (
+    <Fragment>
+      <textarea value={input} onChange={e => setInput(e.target.value)} name="code" placeholder="Please type code here" cols={30} rows={10}></textarea>
+      <div>
+        <button onClick={handleClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </Fragment>
+  )
+}
+
+export default FormElements
